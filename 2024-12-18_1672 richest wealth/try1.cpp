afb119979c8429575ab284f4675f74484a094969
silofox <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>
2024-12-18 15:24:43 +0900

    solve 1672

---
+++ 2024-12-18_1672 richest wealth/try1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max_wealth = 0;
+
+        for(int i = 0; i < accounts.size(); i++) {
+            int each_wealth = 0;
+            for (int j = 0; j < accounts[0].size(); j++) {
+                each_wealth += accounts[i][j];
+            }
+            if (each_wealth > max_wealth) {
+                max_wealth = each_wealth;
+            }
+        }
+
+        return max_wealth;
+    }
+};
